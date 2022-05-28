d610f44672ffa3c2638d09b4b02bc3f98d3c9ff6
Lion <leonardofontan14@gmail.com>
2022-05-28 19:08:25 -0300

    refactore (api/index.ts) : Add endpoint

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,2 +1,6 @@
-// ToDo : Los enpoints en general no utilizan directamente las Clases de los modelos.
-// ToDo : Estos enpoints solo se comunican con los controllers.
+// # Los enpoints en general no utilizan directamente las Clases de los modelos.
+// # Estos enpoints solo se comunican con los controllers.
+import { NextApiResponse } from "next";
+export default (res: NextApiResponse) => {
+  res.status(418).send("Bienvenido");
+};
